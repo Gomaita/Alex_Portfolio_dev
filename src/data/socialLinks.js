a209a2846ba2efaaf446a92d8bc7efef7a58e16c
Gomaita <56037701+Gomaita@users.noreply.github.com>
2026-06-03 12:25:27 +0200

    Add basic SEO metadata

--- src/data/socialLinks.js
+++ src/data/socialLinks.js
@@ -3,5 +3,5 @@ import { appConfig } from '../config/appConfig'
 export const contact = {
   email: appConfig.contactEmail,
   github: 'https://github.com/Gomaita',
-  linkedin: 'https://www.linkedin.com/',
+  linkedin: 'https://www.linkedin.com/in/alex-gomez-lo/',
 }
